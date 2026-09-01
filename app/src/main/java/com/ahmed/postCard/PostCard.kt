be19b0872effce0c1546101205071226bf5d8bc1
Ahmed Dendrawy <ahmeddendriwy11@gmail.com>
2026-09-01 08:34:45 +0300

    enhance card

--- app/src/main/java/com/ahmed/postCard/PostCard.kt
+++ app/src/main/java/com/ahmed/postCard/PostCard.kt
@@ -1,21 +1,11 @@
 package com.ahmed.postCard
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.DragInteraction
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,33 +22,31 @@ fun PostCard(
     publishDate: String,
     content: String,
 ) {
-    Column(
-        modifier
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(24.dp))
-            .background(Color.White)
-            .border(width = 1.dp, color = Color.Black.copy(alpha = 0.08f))
-    ) {
-        PostHeader(
-            modifier = modifier.padding(12.dp),
-            profileImage = profileImage,
-            publisherName = publisherName,
-            publishDate = publishDate
-        )
-        BasicText(
-            text = content,
-            modifier = Modifier
-                .padding(horizontal = 12.dp)
-                .padding(top = 4.dp)
-                .padding(bottom = 12.dp),
-            style = contentStyle
-        )
-        Image(
-            painter = postImage,
-            contentDescription = "Post image",
-            modifier = Modifier.fillMaxWidth()
-        )
-    }
+    Card(
+        modifier = modifier,
+        {
+            PostHeader(
+                modifier = modifier.padding(12.dp),
+                profileImage = profileImage,
+                publisherName = publisherName,
+                publishDate = publishDate
+            )
+            BasicText(
+                text = content,
+                modifier = Modifier
+                    .padding(horizontal = 12.dp)
+                    .padding(top = 4.dp)
+                    .padding(bottom = 12.dp),
+                style = contentStyle
+            )
+            Image(
+                painter = postImage,
+                contentDescription = "Post image",
+                modifier = Modifier.fillMaxWidth()
+            )
+        },
+    )
+
 }
 
 
